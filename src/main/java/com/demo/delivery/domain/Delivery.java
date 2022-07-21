9014010c9c965e59fd8a19c369a3057d52fe8db8
gyong1209 <gyong1209@naver.com>
2022-07-21 16:49:41 +0900

    lombok add

--- src/main/java/com/demo/delivery/domain/Delivery.java
+++ src/main/java/com/demo/delivery/domain/Delivery.java
@@ -11,7 +11,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.PostPersist;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter @Setter
 public class Delivery {
 
     @Id @GeneratedValue
@@ -40,51 +44,5 @@ public class Delivery {
         deliveryCompleted.setOrderItemId(this.orderItemId);
         deliveryCompleted.publishAfterCommit();
     }
-
-
-    public DeliveryStatus getDeliveryStatus() {
-        return deliveryStatus;
-    }
-    public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
-        this.deliveryStatus = deliveryStatus;
-    }
-    public Date getStartDate() {
-        return startDate;
-    }
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-    public Date getEndDate() {
-        return endDate;
-    }
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-    public Long getOrderItemId() {
-        return orderItemId;
-    }
-    public void setOrderItemId(Long orderItemId) {
-        this.orderItemId = orderItemId;
-    }
-    public DeliveryAddress getDeliveryAddress() {
-        return deliveryAddress;
-    }
-    public void setDeliveryAddress(DeliveryAddress deliveryAddress) {
-        this.deliveryAddress = deliveryAddress;
-    }
-    public String getTrackingNumber() {
-        return trackingNumber;
-    }
-    public void setTrackingNumber(String trackingNumber) {
-        this.trackingNumber = trackingNumber;
-    }
-    public String getDeliveryMan() {
-        return deliveryMan;
-    }
-    public void setDeliveryMan(String deliveryMan) {
-        this.deliveryMan = deliveryMan;
-    }
-
-
 
 }
--- src/main/java/com/demo/delivery/domain/PaymentCompleted.java
+++ src/main/java/com/demo/delivery/domain/PaymentCompleted.java
@@ -2,24 +2,14 @@ package com.demo.delivery.domain;
 
 import com.demo.delivery.AbstractEvent;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PaymentCompleted extends AbstractEvent {
 
     private Long id;
     private Long addressId;
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public Long getAddressId() {
-        return addressId;
-    }
-    public void setAddressId(Long addressId) {
-        this.addressId = addressId;
-    }
-
-
 
 }
